a9a7f90f672092ea3e80781ae65f27e01ab20998
kenjpais <kenjpais@gmail.com>
2024-09-26 23:21:40 +0530

    added choice

--- main.go
+++ main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"example.com/go_snips/builder"
+	"example.com/go_snips/factory"
 	"example.com/go_snips/liskov_substitution_principle"
 	"example.com/go_snips/open_closed_principle"
-	"example.com/go_snips/factory"
+	"fmt"
+	"os"
 )
 
 func main() {
-	builder.TestBuilder()
-	open_closed_principle.TestOpenClosed()
-	liskov_substitution_principle.TestShapes()
-	factory.TestFactory()
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: [open_closed_principle | liskov_substitution_principle | builder | factory]")
+		os.Exit(1)
+	}
+
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "open_closed_principle":
+			open_closed_principle.TestOpenClosed()
+		case "liskov_substitution_principle":
+			liskov_substitution_principle.TestShapes()
+		case "builder":
+			builder.TestBuilder()
+		case "factory":
+			factory.TestFactory()
+		default:
+			fmt.Printf("Error: '%s' is not a valid option.\n", arg)
+			fmt.Println("Usage: [open_closed_principle | liskov_substitution_principle | builder | factory]")
+			os.Exit(1)
+		}
+	}
 }
